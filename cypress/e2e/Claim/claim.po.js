e718a35fded1c5a27535f6c7e0359db3210b5916
Kaijar2020 <kizar.cse@gmail.com>
2023-10-13 17:04:48 +0600

    claim page my claim locator find

---
+++ cypress/e2e/Claim/claim.po.js
@@ -0,0 +1,12 @@
+class ClaimPage{
+    claimPageLocator ={
+
+        claimMenu : "a.oxd-main-menu-item.active",
+        myClaims : "//a[contains(text(),'My Claims')]",
+        submitClaim_btn : "//button[normalize-space()='Submit Claim']",
+        selectEvent : "(//div[@class='oxd-select-text-input'])[1]",
+        reMarks : "textarea.oxd-textarea.oxd-textarea--active.oxd-textarea--resize-vertical",
+        creaTe_btn : "//button[normalize-space()='Create']"
+
+    }
+}
