603bce1250370f4301594bf69a769e63cb1360de
Kaijar2020 <kaijarayon@gmail.com>
2024-08-26 17:44:53 +0600

    change report name

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
   reporterOptions: {
     charts: true,
-    reportPageTitle: 'Test Report',
+    reportPageTitle: 'Test Result Report',
     embeddedScreenshots: true,
     inlineAssets: true,
     saveAllAttempts: false,
